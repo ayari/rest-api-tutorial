2313b0387b9532529d04e615707af00d2e19f9df
Tasos Martidis <anastasios.martidis@tiekinetix.com>
2015-12-21 14:23:31 +0100

    Adding documentation for the REST API using swagger

--- src/main/java/com/tasosmartidis/rest_api_tutorial/config/WebConfig.java
+++ src/main/java/com/tasosmartidis/rest_api_tutorial/config/WebConfig.java
@@ -4,15 +4,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc 
 @ComponentScan("com.tasosmartidis.rest_api_tutorial")
 public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	@Override 
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry
+	        .addResourceHandler("swagger-ui.html")
+	        .addResourceLocations("classpath:/META-INF/resources/");
+	    registry
+	        .addResourceHandler("/webjars/**")
+	        .addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 }
